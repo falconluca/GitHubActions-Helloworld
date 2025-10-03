9a983d37e42db427f175f680489f982b8462a8e6
Luca <yangshaoxiong5545@gmail.com>
2025-10-03 11:47:16 +0800

    fix: typo

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
-const isProd = process.env.NODE_ENV === "production";
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
   output: "export",
-  basePath: isProd ? "/GitHubActions-Helloworld" : "",
-  assetPrefix: isProd ? "/GitHubActions-Helloworld/" : "",
 };
 
 export default nextConfig;
